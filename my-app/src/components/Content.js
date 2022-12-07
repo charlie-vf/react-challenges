f621d767f7feb55c5a4dbc2b3a710f661ab4a654
Charlie <charlottevfarmer@gmail.com>
2022-12-07 13:00:45 +0000

    Component Lifecycles challenge

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import {savedPosts} from '../posts.json';
 import css from './css/Content.module.css';
 import PostItem from './PostItem';
+import Loader from './Loader';
 
 export class Content extends Component {
 
     constructor(props) {
         super(props)
 
+        this.state ={
+          isLoaded: false
+        }
+
     }
+
+    componentDidMount() {
+      setTimeout(() => {
+          this.setState({
+              isLoaded: true
+          })
+      }, 2000)
+  }
 
 
   render() {
@@ -32,7 +45,11 @@ export class Content extends Component {
                     
                 }))
             } */}
-            <PostItem savedPosts={savedPosts} />
+            {/* Conditional to load Loader icon when loading data, then display content */}
+            {
+              this.state.isLoaded ? <PostItem savedPosts={savedPosts} />
+              : <Loader />
+            }
         </div>
       </div>
     )
