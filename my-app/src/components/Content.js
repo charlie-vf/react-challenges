7307b9e6913bf8a9a8bdc2fcb3f20b0d23d0ab6f
Charlie <charlottevfarmer@gmail.com>
2022-12-08 12:04:22 +0000

    Search feature challenge

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -10,7 +10,8 @@ export class Content extends Component {
         super(props)
 
         this.state ={
-          isLoaded: false
+          isLoaded: false,
+          posts: []
         }
 
     }
@@ -18,9 +19,20 @@ export class Content extends Component {
     componentDidMount() {
       setTimeout(() => {
           this.setState({
-              isLoaded: true
+              isLoaded: true,
+              posts: savedPosts,
           })
       }, 2000)
+  }
+
+  handleChange = (event) => {
+    const name = event.target.value.toLowerCase()
+    const filteredPosts = savedPosts.filter((post) => {
+      return post.name.toLowerCase().includes(name)
+    })
+    this.setState({
+      posts: filteredPosts
+    })
   }
 
 
@@ -30,6 +42,16 @@ export class Content extends Component {
         
         <div className= {css.TitleBar}>
             <h1>My Photos</h1>
+            <form>
+              <label htmlFor="searchInput">Search</label>
+              <input
+                onChange = {(event) => this.handleChange(event)}
+                type="search"
+                id="searchInput"
+                placeholder="By Author"
+              />
+              <h4>posts found: {this.state.posts.length}</h4>
+            </form>
         
         </div>
         <div className= {css.SearchResults}>
@@ -47,7 +69,7 @@ export class Content extends Component {
             } */}
             {/* Conditional to load Loader icon when loading data, then display content */}
             {
-              this.state.isLoaded ? <PostItem savedPosts={savedPosts} />
+              this.state.isLoaded ? <PostItem savedPosts={this.state.posts} />
               : <Loader />
             }
         </div>
